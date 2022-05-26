8f045901f4242137aae0398234849c9e2a82736c
Ranjeet Dhole <106011540+RanjeetDhole@users.noreply.github.com>
2022-05-26 13:10:27 +0530

    Add files via upload

---
+++ V.java
@@ -0,0 +1,52 @@
+import java.lang.*;
+import java.util.*;
+
+class V
+{
+          
+   public static void main(String arg[])
+      {
+          Vector<Integer> obj = new Vector<Integer>(12);
+
+          obj.add(11);
+          obj.add(20);  
+          obj.add(51);
+          obj.add(101);
+          obj.add(110);
+          obj.add(111);
+
+          System.out.println("Cpacity of vector is : "+obj.capacity());
+          System.out.println("Size of vector is: "+obj.size());
+
+          int i = 0;
+          for(i = 0;i < obj.size();i++)
+          {
+             System.out.println(obj.get(i));
+          }
+
+          int no = 0;int iCnt= 0;
+          for(i = 0;i < obj.size();i++)
+          {
+             no = obj.get(i);
+             if(no % 2 == 0)
+             {
+                       iCnt++;
+             }
+          }
+
+          System.out.println("Even elements are: "+iCnt);
+
+
+          Iterator iobj = obj.iterator();
+          while(iobj.hasNext())
+          {
+             System.out.println(iobj.next());
+          }
+          obj.clear();
+        
+                
+      }
+
+
+}
+          
